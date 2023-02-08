8b0ccf618fdc9bf741fe1f9c557f2c8cc2159cdd
Akash Gajwar <75195665+akashgajwar@users.noreply.github.com>
2023-02-09 03:59:53 +0530

    feat : Home & product

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Home from './Home';
 
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
     <div className="App">
       {/* <h1>Hello clever programer, lets build the Amazon clone app ): </h1> */}
       <Header />
-      {/*Home*/} 
+      <Home />
     </div>
   );
 }
